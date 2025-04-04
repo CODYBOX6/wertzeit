f260940c71248e214207fc691e217247da064168
CODYBOX6 <sonnybox@hotmail.com>
2025-04-04 12:36:37 +0200

    Modification de la section About : déplacement de la présentation et suppression des barres de compétences

--- script.js
+++ script.js
@@ -26,11 +26,15 @@ document.addEventListener('DOMContentLoaded', function() {
                     setTimeout(() => {
                         el.style.opacity = '1';
                         el.style.backgroundPosition = '100% 0';
+                        el.classList.add('animated');
                     }, index * 150);
                 });
             }
         });
-    }, { threshold: 0.3 });
+    }, { 
+        threshold: 0.1,
+        rootMargin: '0px 0px -50px 0px'
+    });
 
     if (aboutSection) observer.observe(aboutSection);
 
